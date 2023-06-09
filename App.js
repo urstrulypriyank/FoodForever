2934f0d7a1c77e771273950c51b897c47865e365
urstrulypriyank <type2email@gmail.com>
2023-06-09 18:37:00 +0530

    FolderStructre setup

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function AppLayout() {
+  return <h1>Priyank Rai</h1>;
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
